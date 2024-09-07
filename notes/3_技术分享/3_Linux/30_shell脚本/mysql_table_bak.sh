3b7dc3566ac7f725f3f7f71a12a07c3b273447cd
genghui <chunhui128@outlook.com>
2024-09-08 00:24:02 +0800

    1

---
+++ notes/3_技术分享/3_Linux/30_shell脚本/mysql_table_bak.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# 设置mysql的登录用户名和密码
+MYSQL_USER="readuser"
+MYSQL_PASSWORD="123123"
+MYSQL_HOST="localhost"
+MYSQL_PORT="3306"
+ 
+# 需要备份的数据库
+DATABASE="test"
+# 保存主目录
+BACKUP_DIR="/home/backup/mysql"
+# 保存子目录
+BACKUP_SUB_DIR="$(date +%Y%m%d)"
+# 保存路径
+BACKUP_LOCATION="${BACKUP_DIR}/${BACKUP_SUB_DIR}"
+# 备份时间
+BACKUP_TIME=$(date +%Y%m%d_%H%M%S)
+
+if [ -z "$BACKUP_LOCATION" ]; then
+  echo "备份文件保存目录不能为空"
+  exit 1
+fi
+# 备份目录不存在则创建
+if [ ! -d "$BACKUP_LOCATION" ]; then
+  mkdir -p "$BACKUP_LOCATION"
+fi
+if [ -z "$DATABASE" ]; then
+  echo "备份数据库名称不能为空"
+  exit 1
+fi
+if [ -z "$1" ]; then
+  echo "备份表名称不能为空"
+  exit 1
+fi
+
+BACKUP_FILE_NAME="${BACKUP_LOCATION}/${DATABASE}_$1_${BACKUP_TIME}.sql.gz"
+
+/usr/local/mysql/bin/mysqldump -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --routines --single-transaction --quick --databases "$DATABASE" --tables "$1" |gzip > "$BACKUP_FILE_NAME"
+
+# 检查备份是否成功
+if [ $? -eq 0 ]; then
+  echo "备份成功: $BACKUP_FILE_NAME"
+  ls -lh "${BACKUP_FILE_NAME}"
+else
+  echo "备份失败"
+  exit 1
+fi
